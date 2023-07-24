61a8413bd5f0c92cc5a661f987400adef4568382
Zachary Ringwood <zringwood@gmail.com>
2023-07-24 16:20:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/middlegames/:id", (req, res) => {
     })
 })
 
-app.listen($PORT, () => {
-    console.log(`Server Listening on port ${$PORT}!`)
+app.listen(8080, () => {
+    console.log(`Server Listening on port ${8080}!`)
 })
 
