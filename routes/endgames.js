9d1477d87f4bd8061877a2fe8ccded70fbeb8c91
Zachary Ringwood <zringwood@gmail.com>
2023-07-27 14:13:19 -0400

    Update endgames.js

--- routes/endgames.js
+++ routes/endgames.js
@@ -1,37 +1,25 @@
 const express = require('express')
 const router = express.Router();
+const { BigQuery } = require('@google-cloud/bigquery');
+const bigqueryClient = new BigQuery();
 
-//Serves an endgame puzzle with a given id
-router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    knex('endgames').where({ id: req.params.id }).then((response) => {
-        if (response.length > 0)
-            return res.status(200).send(response[0])
-        return res.status(404).send(`No puzzle with id ${req.params.id} found!`)
+//Serve an endgame puzzle with a given difficulty
+app.get("/endgames/:difficulty/:id", (req, res) => {
+    const difficulty = req.params.difficulty;
+    const sqlQuery = `SELECT *
+    FROM \`tacticsdb-393323.puzzles.endgames_${difficulty}\`
+    WHERE id = ${Number(req.params.id)}`;
+    const options = {
+        query: sqlQuery,
+        // Location must match that of the dataset(s) referenced in the query.
+        location: 'us-central1',
+    };
+    // Run the query
+    bigqueryClient.query(options).then(response => {
+        res.status(200).send(response[0][0])
     }).catch(error => {
-        res.status(500).send(error);
-    })
-
-})
-//Returns the number of puzzles in the database with a given difficulty
-router.get("/:difficulty/count", (req, res) => {
-    const difficulty = req.params.difficulty;
-    knex.raw(` select count(*) from endgames_${difficulty}`).then(response => {
-        if (response.length > 0)
-            return res.status(200).send(response[0][0])
-        return res.status(404).send(`No puzzle with id ${req.params.id} found!`)
-    }).catch(() => {
-        return res.status(404).send(`No table with that difficulty (${req.params.difficulty}) found!`)
-    })
-})
-//Serves a endgame puzzle with a given difficulty and a given id. 
-router.get("/:difficulty/:id", (req, res) => {
-    const id = req.params.id;
-    const difficulty = req.params.difficulty;
-    knex.raw(` select * from endgames_${difficulty} where id = ${id}`).then(response => {
-        if (response.length > 0)
-            return res.status(200).send(response[0][0])
-        return res.status(404).send(`No puzzle with id ${req.params.id} found!`)
+        console.log(error)
+        res.sendStatus(404)
     })
 })
 module.exports = router;
