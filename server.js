974f28bf80163ed902bcce568a85c5ca783f680d
Zachary Ringwood <zringwood@gmail.com>
2023-07-28 17:32:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -25,14 +25,14 @@ app.get("/introduction/easy/:id", (req, res) => {
     const id = req.params.id
     const puzzles = [
         {
-            Moves: "b5a5 d3b5", 
-            FEN: "8/3B4/8/1k6/8/3Q4/8/6K1 b - - 1 1", 
-            Themes: "checkmateTheKing!"
+            moves: "b5a5 d3b5", 
+            fen: "8/3B4/8/1k6/8/3Q4/8/6K1 b - - 1 1", 
+            themes: "checkmateTheKing!"
         },
         {
-            Moves: "b7c8 d7c8", 
-            FEN: "8/1b1P4/8/1k3K2/8/8/8/8 b - - 1 1", 
-            Themes: "promoteThePawn!"
+            moves: "b7c8 d7c8", 
+            fen: "8/1b1P4/8/1k3K2/8/8/8/8 b - - 1 1", 
+            themes: "promoteThePawn!"
         }
     ]
     if(id < 1 || id > puzzles.length){
